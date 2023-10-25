0e71848574c426a02f3941698f7013abdd72f6e6
JordanTitus <jordantitus2301@gmail.com>
2023-10-25 09:56:15 +0200

    Add project files.

---
+++ Jordan-Titus-ST10082034-PROG7312/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jordan_Titus_ST10082034_PROG7312
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Library/Library1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Timers;
+
+namespace Library
+{
+    public class Library1
+    {
+        private Timer timer;
+
+        /// <summary>
+        ///     Holds the alapsed time in second
+        /// </summary>
+        private int elapsedTimeInSeconds;
+
+        /// <summary>
+        ///     Store a TimeSpan Value
+        /// </summary>
+        private TimeSpan remainingTime;
+
+        public event EventHandler<ElapsedEventArgs> Elapsed;
+
+        public Library1(int interval)
+        {
+            timer = new Timer();
+            timer.Interval = interval;
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            elapsedTimeInSeconds++;
+            OnElapsed(new ElapsedEventArgs(elapsedTimeInSeconds));
+        }
+
+        // ------------------------------------------------------------------ //
+        /// <summary>
+        ///     Start timer
+        /// </summary>
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        // ------------------------------------------------------------------ //
+        /// <summary>
+        ///     Stops timer
+        /// </summary>
+        public void Stop()
+        {
+            timer.Stop();
+        }
+
+        // ------------------------------------------------------------------ //
+        /// <summary>
+        ///     Pauses timer
+        /// </summary>
+        public void Pause()
+        {
+            remainingTime = TimeSpan.FromMilliseconds(timer.Interval);
+            timer.Stop();
+        }
+
+        // ------------------------------------------------------------------ //
+        /// <summary>
+        ///     Resumes timer from paused time
+        /// </summary>
+        public void Resume()
+        {
+            try
+            {
+
+                timer.Interval = (int)remainingTime.TotalMilliseconds;
+
+            }
+            catch (ArgumentException)
+            {
+
+            }
+            timer.Start();
+        }
+
+        // ------------------------------------------------------------------ //
+        /// <summary>
+        ///     Ticks the timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            elapsedTimeInSeconds++;
+            OnElapsed(new ElapsedEventArgs(elapsedTimeInSeconds));
+        }
+
+        // ------------------------------------------------------------------ //
+        /// <summary>
+        ///     triggers elapsed event
+        /// </summary>
+        /// <param name="e"></param>
+        protected virtual void OnElapsed(ElapsedEventArgs e)
+        {
+            Elapsed?.Invoke(this, e);
+        }
+    }
+
+    public class ElapsedEventArgs : EventArgs
+    {
+        public int ElapsedTimeInSeconds { get; }
+
+        public ElapsedEventArgs(int elapsedTimeInSeconds)
+        {
+            ElapsedTimeInSeconds = elapsedTimeInSeconds;
+        }
+    }
+}
